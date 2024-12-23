a7728383c059144a9ee2a51dbb44e6c8e02b7e74
Nikhil-2x <nikhilyadav101513@gmail.com>
2024-12-24 00:51:22 +0530

    created this folder

---
+++ bmi-calci/script.js
@@ -0,0 +1,18 @@
+const forms = document.querySelector("form");
+
+forms.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const height = document.querySelector("#height").value;
+  const weight = document.querySelector("#weight").value;
+  const results = document.querySelector("#results");
+
+  if (height === "" || height <= 0 || isNaN(height)) {
+    alert("pls enter valid height");
+  } else if (weight === "" || weight <= 0 || isNaN(weight)) {
+    alert("pls enter valid weight");
+  } else {
+    const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+    results.innerHTML = `<span>${bmi}</span>`;
+  }
+});
